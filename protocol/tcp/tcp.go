0f3234e8d9d903940c6611f9d739cf3351570058
dd <realxhyonline@gmail.com>
2024-10-13 22:52:08 +0800

    修改协议栈

--- protocol/tcp/tcp.go
+++ protocol/tcp/tcp.go
@@ -2,9 +2,10 @@ package tcp
 
 import (
 	"github.com/gogf/gf/v2/encoding/gbinary"
-	"github.com/xhyonline/hs_protocol/code"
 	"io"
+	"math"
 	"net"
+	"sort"
 	"time"
 )
 
@@ -13,7 +14,7 @@ const (
 )
 
 const (
-	BufferSize = 4096
+	MaxPayload = 4096
 )
 const (
 	ReaderUnlimited = -1 // 无数次读取
@@ -35,14 +36,10 @@ func (s *DataFragment) Encode() []byte {
 	return gbinary.Encode(s.GlobalSeq, s.SubSeq, s.IsEnd, s.Control, s.PayloadLength, s.Payload)
 }
 
-// 协议读取器,读取 N 次
-func Reader(conn net.Conn, times int, timeout time.Duration, callback func(fragment *DataFragment)) error {
-	var readCnt int
+// Reader 协议读取器。当 callback 返回 false 则退出读取
+func Reader(conn net.Conn, timeout time.Duration, callback func(fragment *DataFragment) bool) error {
 	connReaderIns := newConnReader(conn, timeout)
 	for {
-		if times != ReaderUnlimited && readCnt > times {
-			return code.NewCodeError(code.ReadTimesBreak)
-		}
 		header := make([]byte, FixedLengthDataFragment)
 		// 读满整个头部
 		_, err := io.ReadFull(connReaderIns, header)
@@ -66,7 +63,99 @@ func Reader(conn net.Conn, times int, timeout time.Duration, callback func(fragm
 			return err
 		}
 		ins.Payload = payloadBody
-		callback(ins)
-		readCnt++
+		if !callback(ins) {
+			break
+		}
 	}
+	return nil
+}
+
+// SendMsg 发送数据
+func SendMsg(conn net.Conn, globalSeq uint32, controlFlag uint16, payload []byte) error {
+	var subSeq uint32 = 1
+	cutCount := int(math.Ceil(float64(len(payload)) / float64(MaxPayload)))
+	maxPayload := MaxPayload
+	if maxPayload > len(payload) {
+		maxPayload = len(payload)
+	}
+	var startOffset = 0
+	for i := 0; i < cutCount; i++ {
+		var isEnd = true
+		payloadFrag := payload[startOffset:]
+		if i != cutCount-1 {
+			payloadFrag = payload[startOffset : startOffset+maxPayload]
+			isEnd = false
+		}
+		data := &DataFragment{
+			GlobalSeq:     globalSeq,
+			SubSeq:        subSeq,
+			IsEnd:         isEnd,
+			Control:       controlFlag,
+			PayloadLength: uint32(len(payloadFrag)),
+			Payload:       payloadFrag,
+		}
+		subSeq++
+		startOffset += maxPayload
+		if _, err := conn.Write(data.Encode()); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SendMsgWithRelay 发送消息并且返回一条完整的协议包数据,要求来回响应是要相同的 globalSeq
+// 用于登录、权限等单次认证。一条流要保证数据要线性
+func SendMsgWithRelay(conn net.Conn, globalSeq uint32,
+	controlFlag uint16, payload []byte, timeout time.Duration) (*DataFragment, error) {
+	if err := SendMsg(conn, globalSeq, controlFlag, payload); err != nil {
+		return nil, err
+	}
+	var fragmentArray = make([]*DataFragment, 0)
+	var endSubSeq bool
+	if err := Reader(conn, timeout, func(fragment *DataFragment) bool {
+		if fragment.GlobalSeq != globalSeq {
+			return true
+		}
+		fragmentArray = append(fragmentArray, fragment)
+		if fragment.IsEnd {
+			endSubSeq = true
+		}
+		if !endSubSeq {
+			return true
+		}
+		sort.SliceStable(fragmentArray, func(i, j int) bool {
+			return fragmentArray[i].SubSeq < fragmentArray[j].SubSeq
+		})
+		// 判断数据包是否顺序
+		var prev uint32 = 0
+		for _, v := range fragmentArray {
+			if prev == 0 {
+				prev = v.SubSeq
+				continue
+			}
+			// 非连续,继续读。还有数据
+			if v.SubSeq != prev+1 {
+				return true
+			}
+			prev = v.SubSeq
+		}
+		return false
+	}); err != nil {
+		return nil, err
+	}
+	lastFragment := fragmentArray[len(fragmentArray)-1]
+	// 组成一个 Fragment 返回
+	dataFragment := &DataFragment{
+		GlobalSeq:     globalSeq,
+		SubSeq:        lastFragment.SubSeq,
+		IsEnd:         true,
+		Control:       lastFragment.Control,
+		PayloadLength: 0,
+		Payload:       make([]byte, 0),
+	}
+	for _, v := range fragmentArray {
+		dataFragment.Payload = append(dataFragment.Payload, v.Payload...)
+	}
+	dataFragment.PayloadLength = uint32(len(dataFragment.Payload))
+	return dataFragment, nil
 }
