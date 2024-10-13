da85264e182be820ff3bd96de259385df4a12fe2
dd <realxhyonline@gmail.com>
2024-10-13 17:47:43 +0800

    新增控制报文

--- protocol/tcp/control.go
+++ protocol/tcp/control.go
@@ -2,6 +2,8 @@ package tcp
 
 // 控制协议报文
 const (
-	ControlSign = iota + 1 // 发送登录信息的报文。账号密码不正确的情况下不允许用户连接
-	ControlPing            // 探活报文
+	ControlSign        = iota + 1 // 发送登录信息的报文。账号密码不正确的情况下不允许用户连接
+	ControlSignSuccess            // 登录成功
+	ControlSignError              // 登录失败
+	ControlPing                   // 探活报文
 )
